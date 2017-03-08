e85ab2342f043c49b614f36f24e1123e26ddb9d5
Grey Patterson <greypatterson@gmail.com>
2017-03-08 11:32:58 -0800

    More buttons

--- StackViews/ViewController.swift
+++ StackViews/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
