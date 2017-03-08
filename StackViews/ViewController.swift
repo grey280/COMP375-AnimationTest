1c38bd3325c00406b983a5ff104635e06725b1e9
Grey Patterson <greypatterson@gmail.com>
2017-03-08 11:42:04 -0800

    Show and hide the other views

--- StackViews/ViewController.swift
+++ StackViews/ViewController.swift
@@ -11,8 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func settingsButtonPressed(_ sender: UIButton) {
-        
+        for emojiView in emojiViews{
+            emojiView.isHidden = !emojiView.isHidden
+        }
     }
+    @IBOutlet var emojiViews: [UIView]!{
+        didSet{
+            for emojiView in emojiViews{
+                emojiView.isHidden = true
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
