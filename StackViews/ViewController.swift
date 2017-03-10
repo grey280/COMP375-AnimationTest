7d1e92fadffd20855282d4a45e415f7239631897
Grey Patterson <greypatterson@gmail.com>
2017-03-10 11:13:35 -0800

    Factor out a function

--- StackViews/ViewController.swift
+++ StackViews/ViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func settingsButtonPressed(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.5) { 
+        toggleEmojiViews(sender)
+    }
+    
+    @IBAction func toggleEmojiViews(_ sender: UIButton){
+        UIView.animate(withDuration: 0.5) {
             self.emojiViews.forEach{
                 $0.isHidden = !$0.isHidden
             }
         }
     }
+    
     @IBOutlet var emojiViews: [UIView]!{
         didSet{
             emojiViews.forEach{
